0c3ea92196b3d25b5cb36723fb371f862053052e
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-05-16 16:28:27 -0700

    Serialize WIP

--- CanSum.java
+++ CanSum.java
@@ -1,5 +1,6 @@
 public class CanSum {
     /**
+     * Problem from 5/14/22:
      * Given a list of numbers and a number k, return whether any two numbers from
      * the list add up to k.
      * 
---
+++ Serialize.java
@@ -0,0 +1,43 @@
+public class Serialize {
+    /**
+     * Problem from 5/16/22:
+     * Given the root to a binary tree, implement serialize(root), which serializes
+     * the tree into a string, and deserialize(s), which deserializes the string 
+     * back into the tree.
+     */
+
+    /**
+     * plan:
+     * - use some form of binary tree traversal; not sure which is best for
+     *   reconstructing the tree
+     *   - serialize and deserialize are inverses, so it doesn't matter what 
+     *     traversal we use, as long as they both use the same traversal logic
+     *   - need to have indicators/labels since this isn't necesarily balanced
+     *     e.g. "a b c" can be construed as a balanced tree or a skewed tree
+     *     while both using the same traversal method 
+     * - write a basic Node class
+     */
+
+    protected class Node {
+        String value;
+        Node left;
+        Node right;
+
+        private Node(String value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        // [] do we need helper functions?
+    }
+
+    public String serialize(Node node) {
+        
+        return "";
+    }
+
+    public Node deserialize(String serialization) {
+        return new Node();
+    }
+}
