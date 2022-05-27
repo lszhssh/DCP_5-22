b83727918e7df757cf0cf3cd4c30cdcd4cb96113
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-05-27 13:33:11 -0700

    Complete naive Autocomplete

--- Autocomplete.java
+++ Autocomplete.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Autocomplete {
     // Problem from 5/24/22:
 
@@ -8,4 +10,33 @@ public class Autocomplete {
     // [dog, deer, deal], return [deer, deal].
     // Hint: Try preprocessing the dictionary into a more efficient 
     // data structure to speed up queries.
+
+    // Solution time: 11 minutes (naive solution)
+
+    // Plan:
+    // - Isn't this solved easily by using String.indexOf()?
+        // Yes, but we can optimize it with something called a trie[sic].
+        // [] How to implement a trie?
+
+    public ArrayList<String> autocomplete(String query, String[] possibleStrings) {
+        ArrayList<String> suggestions = new ArrayList<>();
+        for (int i = 0; i < possibleStrings.length; i++) {
+            if (possibleStrings[i].indexOf(query) == 0) {
+                suggestions.add(possibleStrings[i]);
+            }
+        }
+        return suggestions;
+    }
+
+    public void printArrayList(ArrayList<String> strs) {
+        for (String str : strs) {
+            System.out.print(str + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Autocomplete obj = new Autocomplete();
+        String[] str = new String[] {"dog", "deer", "deal"};
+        obj.printArrayList(obj.autocomplete("de", str));
+    }
 }
--- JobScheduler.java
+++ JobScheduler.java
@@ -3,4 +3,6 @@ public class JobScheduler {
 
     // Implement a job scheduler which takes in a function f and an integer n, 
     // and calls f after n milliseconds.
+
+    // Uses the concepts of threading in OS development.
 }
