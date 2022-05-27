7a57fb4c3ad7067411ffac3ccd73312acfadc5be
StevenLi007 <105518183+lszhssh@users.noreply.github.com>
2022-05-27 15:58:15 -0700

    Progress on implementing trie for Autocomplete

--- Autocomplete.java
+++ Autocomplete.java
@@ -16,7 +16,7 @@ public class Autocomplete {
     // Plan:
     // - Isn't this solved easily by using String.indexOf()?
         // Yes, but we can optimize it with something called a trie[sic].
-        // [] How to implement a trie?
+        // [X] How to implement a trie?
 
     public ArrayList<String> autocomplete(String query, String[] possibleStrings) {
         ArrayList<String> suggestions = new ArrayList<>();
@@ -34,9 +34,70 @@ public class Autocomplete {
         }
     }
 
+    static class TrieNode {
+        char data;
+        ArrayList<TrieNode> children;
+        boolean isWord;
+
+        public TrieNode(char data) {
+            this.data = data;
+            this.children = new ArrayList<>();
+            this.isWord = false;
+        }
+    }
+
+    TrieNode root;
+
+    public Autocomplete(TrieNode root) {
+        this.root = root;
+    }
+
+    public void add(String[] strs) {
+        for (String str : strs) {
+            TrieNode currNode = this.root;
+            for (int i = 0; i < str.length() - 1; i++) {
+                char currChar = str.charAt(i);
+                if (!currNode.children.contains(currChar)) {
+                    TrieNode newNode = new TrieNode(currChar);
+                    currNode.children.add(newNode);
+                    currNode = newNode;
+                } else {
+                    for (TrieNode n : currNode.children) {
+                        if (n.data == currChar) {
+                            currNode = n;
+                        }
+                    }
+                }
+            }
+            currNode.isWord = true;
+        }
+    }
+
+    public ArrayList<String> trieAutocomplete(String prefix, String[] strs) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String str : strs) {
+            TrieNode currNode = this.root;
+            boolean startsWith = true;
+            for (int i = 0; i < str.length(); i++) {
+                char currChar = str.charAt(i);
+                if (!currNode.children.contains(currChar)) {
+                    startsWith = false;
+                    break;
+                }
+            }
+            if (startsWith) {
+                result.add(str);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        Autocomplete obj = new Autocomplete();
-        String[] str = new String[] {"dog", "deer", "deal"};
-        obj.printArrayList(obj.autocomplete("de", str));
+        TrieNode n1 = new TrieNode('n');
+        Autocomplete obj = new Autocomplete(n1);
+        String[] strs = new String[] {"dog", "deer", "deal"};
+        // obj.printArrayList(obj.autocomplete("de", strs));
+        obj.add(strs);
+        obj.printArrayList(obj.trieAutocomplete("de", strs));
     }
 }
